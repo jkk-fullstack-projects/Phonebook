1c58fdd0c4712ae50de4d827d70154fd29ea1b9f
JouniGitti <kalaksi@gmail.com>
2024-02-26 09:29:41 +0200

    Transferred searchform inputfield tailwindcss styling to PhonebookStyles.jsx

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,19 +1,20 @@
-    const SearchForm = ({handleFilterChange, filterNames}) => {
-        return (
-            <>
-                <h4 className="text-l font-bold mb-2 text-gray-900">Show names beginning with:</h4>
-                <div className="bg-blue-100 p-4 rounded-lg mb-4">
-                    <form className="space-y-4">
-                    <input 
-                        className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                        onChange={handleFilterChange}
-                        value={filterNames}>
-                    </input>
-                    </form>
-                </div>
-            </>
-        )
-    };
-    
-    export default SearchForm;
+import { inputStyles } from '../styles/PhonebookStyles.jsx'
+const SearchForm = ({handleFilterChange, filterNames}) => {
+    return (
+        <>
+            <h4 className="text-l font-bold mb-2 text-gray-900">Show names beginning with:</h4>
+            <div className="bg-blue-100 p-4 rounded-lg mb-4">
+                <form className="space-y-4">
+                <input 
+                    className={inputStyles.searchFormInput}
+                    onChange={handleFilterChange}
+                    value={filterNames}>
+                </input>
+                </form>
+            </div>
+        </>
+    )
+};
+
+export default SearchForm;
     
--- src/styles/PhonebookStyles.jsx
+++ src/styles/PhonebookStyles.jsx
@@ -15,6 +15,7 @@ export const buttonStyles = {
 
 export const inputStyles = {
     inputField: "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline",
+    searchFormInput: "mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
 };
 
 export const listStyles = {
